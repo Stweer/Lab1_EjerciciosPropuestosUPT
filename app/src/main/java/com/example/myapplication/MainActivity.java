09136cc3b13efc63bc1fe1d1b94e26c939008336
Stweer <38274756+Stweer@users.noreply.github.com>
2020-01-12 21:58:07 -0500

    Cofiguracion MainActivity, Asignacion de accion boton de Equal (=) , integrando motor de Javascrit mozilla rhino para las operaciones.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -172,6 +172,7 @@ public class MainActivity extends AppCompatActivity {
                 tvInput.setText(process + "%");
             }
         });
+        //Validando accion de "(" si esta en process se completa con ")"
         btnBracket.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -187,7 +188,27 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        // Accion de Igual , integracion de motor de javascript mozilla rhino
+        btnEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process = tvInput.getText().toString();
+                process = process.replaceAll("x","x");
+                process = process.replaceAll("%","/100");
+                process = process.replaceAll("/","/");
 
+                Context rhino = Context.enter();
+                rhino.setOptimizationLevel(-1);
+                String finalResult = "";
+                try {
+                    Scriptable scriptable = rhino.initSafeStandardObjects();
+                    finalResult = rhino.evaluateString(scriptable,process,"javascript",1,null).toString();
+                }catch (Exception e){
+                    finalResult="0";
+                }
+                tvOutput.setText(finalResult);
+            }
+        });
 
     }
 }
