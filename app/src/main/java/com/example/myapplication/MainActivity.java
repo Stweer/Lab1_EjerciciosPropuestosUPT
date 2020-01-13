7435b4fd68f91f29fb99a23bf50981c241509af0
Stweer <38274756+Stweer@users.noreply.github.com>
2020-01-12 20:44:13 -0500

    Cofiguracion MainActivity, declaracion de los Buttons,TextViews e Variables

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,55 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
+    Button btnPlus,btnMinus,btnMultiply,btnDivision,btnEqual,btnClear,btnPercent,btnDot,btnBracket;
+    TextView tvInput,tvOutput;
+    String process;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+
+        btnPlus = findViewById(R.id.btnPlus);
+        btnMinus = findViewById(R.id.btnMinus);
+        btnDivision = findViewById(R.id.btnDivision);
+        btnMultiply = findViewById(R.id.btnMultiply);
+
+        btnEqual = findViewById(R.id.btnEqual);
+
+        btnClear = findViewById(R.id.btnClear);
+        btnDot = findViewById(R.id.btnDot);
+        btnPercent = findViewById(R.id.btnPercent);
+        btnBracket = findViewById(R.id.btnBracket);
+
+        tvInput = findViewById(R.id.tvInput);
+        tvOutput = findViewById(R.id.tvOutput);
+        //Boton para limpiar campos de la vista
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvInput.setText("");
+                tvOutput.setText("");
+            }
+        });
     }
 }
